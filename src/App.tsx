af8b82a465de740b67bd4941cd4817e850159773
Lucy by MonstarX <lucy@lucyagent.com>
2026-03-05 14:44:01 +0000

    feat: Add loading spinner
    Generated by LucyAgent

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
+      <div className="flex flex-col items-center gap-4">
+        <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        <p>Start prompting (or editing) to see magic happen :)</p>
+      </div>
     </div>
   )
 }
